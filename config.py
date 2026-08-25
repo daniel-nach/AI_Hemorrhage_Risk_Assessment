41bd276f45744777507deac5cbbd04a8fdba3a1c
Daniel <daniel.nachimson@gmail.com>
2026-08-25 14:32:12 -0400

    current version of the project, including markov model, llm calls and automatically determined patioents. includes citations

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-MODEL_NAME = "llama-3.3-70b-versatile"
+MODEL_NAME = "openai/gpt-oss-20b"
 
 PATIENTS_FILE = os.path.join(BASE_DIR, "data", "mock_data_new.xlsx")
 OUTPUT_CSV = os.path.join(BASE_DIR, "output", "recommendations.csv")
